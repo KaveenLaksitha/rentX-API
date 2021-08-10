6b15bd5de489e0809b833af9acb97eb6ceddb5c8
Kaveen LK <kmklaksitha@gmail.com>
2021-08-10 14:40:01 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const port = 4000;
+
+const URL = process.env.MONGODB_URL;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect(URL, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopologyL: true,
+    useFindandModify: false
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("Mongodb Connection success!");
+})
+
+app.listen(port, () => {
+    console.log(`Server Is Running on Port: ${port}`);
+});
