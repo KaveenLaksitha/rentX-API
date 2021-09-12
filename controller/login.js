700839d60b864e5bed15f17f06f0e8aaa06bb181
Kaveen LK <kmklaksitha@gmail.com>
2021-09-13 01:45:15 +0530

    lgin  controller updated

--- controller/login.js
+++ controller/login.js
@@ -7,8 +7,11 @@ router.route("/get/:un/:pass").get(async (req, res) => {
     let UN = req.params.un;//username taken from the frontend login form
     let pass = req.params.pass;//password taken from the frontend login form
 
+    console.log("un n pass", { UN, pass })
+
     const user = await Login.findOne({ username: UN, password: pass })
         .then((user) => {
+            console.log("dataaaaaaa", user)
             res.status(200).send({ status: "User fetched", login: user })
         }).catch(() => {
             console.log(err.message);
