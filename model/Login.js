52522a5bdb5e199c41e1c30eab46256aa43eaee5
Kaveen LK <kmklaksitha@gmail.com>
2021-10-04 14:14:33 +0530

    missing packages installed

--- model/Login.js
+++ model/Login.js
@@ -3,20 +3,20 @@ const Schema = mongoose.Schema;
 
 const loginSchema = new Schema({
 
-    username :{
-        type:String,
-        required:true,
-        unique:true
+    username: {
+        type: String,
+        required: true,
+        unique: true
     },
 
-    password : {
-        type:String,
-        required:true,
-        unique:true
+    password: {
+        type: String,
+        required: true,
+        unique: true
     }
-  
+
 })
 
-const Login = mongoose.model("Login",loginSchema);
+const Login = mongoose.model("Login", loginSchema);
 
 module.exports = Login;
